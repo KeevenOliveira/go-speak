f1452483ca8d44c52f63523df21fa3eb5ef8b744
Keeven Oliveira <keevinhuholiveira@gmail.com>
2025-01-05 06:17:44 -0300

    [FEAT]: Add home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import Input from "@/components/Input";
+"use client";
+
+import { Suspense, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import ChatsMenu from "@/components/ChatsMenu";
+import Chat from "@/components/Chat";
+import { Container, ChatContainer } from "./styles";
 
 export default function Home() {
+  const { data: _, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [status, router]);
+
   return (
-    <main>
-      <Input />
-    </main>
+    <Container>
+      <aside>
+        <ChatsMenu />
+      </aside>
+      <ChatContainer>
+        <Suspense>
+          <Chat />
+        </Suspense>
+      </ChatContainer>
+    </Container>
   );
 }
