fd55a662dc5d29a1d98f4a5253a52372c418e415
Keeven Oliveira <keevinhuholiveira@gmail.com>
2025-01-05 06:17:11 -0300

    [FEAT]: Add GitHub OAuth integration

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: String(process.env.GITHUB_CLIENT_ID),
+      clientSecret: String(process.env.GITHUB_CLIENT_SECRET),
+    }),
+  ],
+  secret: process.env.NEXT_AUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
